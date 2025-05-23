f75b464f44a5f47bd54d13016ad48600e6f96b82
Daniel Alencar Barros Tavares <danieldgt@gmail.com>
2025-05-22 23:58:17 -0300

    Update le_dht22.py

--- le_dht22.py
+++ le_dht22.py
@@ -4,57 +4,14 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-DHT_PIN = 7  # Pino físico 7 (GPIO 4)
+PIN = 7  # Físico 7 = GPIO 4
 
-def pulse_time():
-    GPIO.setup(DHT_PIN, GPIO.OUT)
-    GPIO.output(DHT_PIN, GPIO.LOW)
-    time.sleep(0.02)  # 20ms de sinal baixo para iniciar
-    GPIO.output(DHT_PIN, GPIO.HIGH)
-    GPIO.setup(DHT_PIN, GPIO.IN)
+GPIO.setup(PIN, GPIO.IN)
 
-    print("Esperando resposta do sensor...")
+print("Monitorando mudanças no pino por 5 segundos...")
 
-    # Espera pull down
-    while GPIO.input(DHT_PIN) == GPIO.HIGH:
-        pass
-
-    # Espera pull up
-    while GPIO.input(DHT_PIN) == GPIO.LOW:
-        pass
-
-    # Espera fim do sinal de resposta
-    while GPIO.input(DHT_PIN) == GPIO.HIGH:
-        pass
-
-    print("Lendo pulsos:")
-
-    timings = []
-    for i in range(40):
-        # LOW
-        while GPIO.input(DHT_PIN) == GPIO.LOW:
-            pass
-
-        t_start = time.time()
-
-        # HIGH
-        while GPIO.input(DHT_PIN) == GPIO.HIGH:
-            pass
-
-        t_end = time.time()
-
-        pulse_len = (t_end - t_start) * 1000000  # microssegundos
-        timings.append(pulse_len)
-
-    return timings
-
-try:
-    while True:
-        pulses = pulse_time()
-        print("Pulsos (µs):")
-        for i, p in enumerate(pulses):
-            print("Bit {}: {:.1f} µs".format(i+1, p))
-        time.sleep(2)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+start_time = time.time()
+while time.time() - start_time < 5:
+    val = GPIO.input(PIN)
+    print("GPIO está: {}".format(val))
+    time.sleep(0.05)
