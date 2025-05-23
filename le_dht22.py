f66c6bec7976b06374998c9dc5413b07be95cec8
Daniel Alencar Barros Tavares <danieldgt@gmail.com>
2025-05-22 23:54:51 -0300

    Update le_dht22.py

--- le_dht22.py
+++ le_dht22.py
@@ -1,75 +1,59 @@
 import ASUS.GPIO as GPIO
 import time
 
-# Suprime avisos como "This channel is already in use"
 GPIO.setwarnings(False)
-
-# Define o modo de numeração de pinos como físico (BOARD)
 GPIO.setmode(GPIO.BOARD)
 
 DHT_PIN = 7  # Pino físico 7 (GPIO 4)
 
-def read_dht22(pin):
-    data = []
+def pulse_time():
+    GPIO.setup(DHT_PIN, GPIO.OUT)
+    GPIO.output(DHT_PIN, GPIO.LOW)
+    time.sleep(0.02)  # 20ms de sinal baixo para iniciar
+    GPIO.output(DHT_PIN, GPIO.HIGH)
+    GPIO.setup(DHT_PIN, GPIO.IN)
 
-    # Sinal de início
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(0.02)  # 20ms
-    GPIO.output(pin, GPIO.HIGH)
-    GPIO.setup(pin, GPIO.IN)
+    print("Esperando resposta do sensor...")
 
-    # Espera a resposta do sensor
-    count = 0
-    while GPIO.input(pin) == GPIO.HIGH:
-        count += 1
-        if count > 10000:
-            print("Timeout esperando resposta do sensor (pull down)")
-            return None
+    # Espera pull down
+    while GPIO.input(DHT_PIN) == GPIO.HIGH:
+        pass
 
-    while GPIO.input(pin) == GPIO.LOW:
-        continue
-    while GPIO.input(pin) == GPIO.HIGH:
-        continue
+    # Espera pull up
+    while GPIO.input(DHT_PIN) == GPIO.LOW:
+        pass
 
-    # Lê 40 bits (5 bytes)
+    # Espera fim do sinal de resposta
+    while GPIO.input(DHT_PIN) == GPIO.HIGH:
+        pass
+
+    print("Lendo pulsos:")
+
+    timings = []
     for i in range(40):
-        while GPIO.input(pin) == GPIO.LOW:
-            continue
-        t = time.time()
-        while GPIO.input(pin) == GPIO.HIGH:
-            continue
-        if time.time() - t > 0.00005:
-            data.append(1)
-        else:
-            data.append(0)
+        # LOW
+        while GPIO.input(DHT_PIN) == GPIO.LOW:
+            pass
 
-    # Converte os bits em valores
-    humidity_bit = data[0:8]
-    humidity_point_bit = data[8:16]
-    temperature_bit = data[16:24]
-    temperature_point_bit = data[24:32]
-    check_bit = data[32:40]
+        t_start = time.time()
 
-    humidity = int("".join([str(i) for i in humidity_bit]), 2)
-    temperature = int("".join([str(i) for i in temperature_bit]), 2)
-    check = int("".join([str(i) for i in check_bit]), 2)
+        # HIGH
+        while GPIO.input(DHT_PIN) == GPIO.HIGH:
+            pass
 
-    if ((humidity + temperature) & 0xFF) != check:
-        print("Checksum inválido")
-        return None
+        t_end = time.time()
 
-    return humidity, temperature
+        pulse_len = (t_end - t_start) * 1_000_000  # microssegundos
+        timings.append(pulse_len)
 
-# Loop de leitura
+    return timings
+
 try:
     while True:
-        result = read_dht22(DHT_PIN)
-        if result:
-            humidity, temperature = result
-            print("Temperatura: {}°C, Umidade: {}%".format(temperature, humidity))
-        else:
-            print("Falha na leitura.")
+        pulses = pulse_time()
+        print("Pulsos (µs):")
+        for i, p in enumerate(pulses):
+            print("Bit {}: {:.1f} µs".format(i+1, p))
         time.sleep(2)
 
 except KeyboardInterrupt:
