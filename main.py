38aa5c8e5e2bbe12349a0a711be9133687f7c033
Daniel Alencar Barros Tavares <danieldgt@gmail.com>
2025-05-31 12:00:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import display
-import le_dht22  # isso já inicia as threads de sensores e LEDs
+import le_dht22
 import time
 
-# Inicia o display e a thread de troca de páginas
+# Inicia o display e dados fixos
 display.iniciar_display()
-
-# Atualiza dados da página 3 (informações do Gecko)
 display.atualizar_gecko(nome="Maracujá", idade="6 meses")
-
-# Atualiza dados da página 2 (status dos relés)
 display.atualizar_reles({
-    'R1': True,   # ligado
-    'R2': False,  # desligado
+    'R1': True,
+    'R2': False,
     'R3': True,
     'R4': False
 })
 
-# Apenas mantém o programa rodando
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
+# Inicia as threads
+thread_leds = le_dht22.thread_leds()
+thread_sensores = le_dht22.thread_sensores()
+
+thread_leds.start()
+thread_sensores.start()
+
+# Mantém vivo o main
+while True:
+    time.sleep(1)
